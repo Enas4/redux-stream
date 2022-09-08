4ff606c0532522023029c3cb8754e3d8b32e2462
Enas4 <harleydavidson789@icloud.com>
2022-09-08 18:10:35 +0500

    changed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 import { createStore, applyMiddleware, compose } from "redux";
 import reducers from "./reducers";
 import App from "./components/App";
 
-const composeEnhancers = window.__;
-REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
